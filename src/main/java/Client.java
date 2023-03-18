fdfe0cab9b6850d8168c3ad160069cea6ed32e5f
gwend <gwendal.hannecart@gmail.com>
2023-03-17 21:09:32 -0400

    Client Java modification et nouveau protobuf

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -8,8 +8,8 @@ public class Client {
     public static void main(String[] args) {
         System.out.println("Bonjour");
         ManagedChannel channel = ManagedChannelBuilder.forAddress("grpc.stymi.fr", 50051).usePlaintext().build();
-        imcServiceGrpc.imcServiceBlockingStub stub= imcServiceGrpc.newBlockingStub(channel);
-        imcResponse reply = stub.iMCRequest(imcPersonneRequest.newBuilder().setAge(45).setSize(14).setStrName("BonjourJava").setWeight(45).build());
+        imcPersonne.imcServiceGrpc.imcServiceBlockingStub stub= imcPersonne.imcServiceGrpc.newBlockingStub(channel);
+        imcPersonne.imcResponse reply = stub.iMCRequest(imcPersonne.imcPersonneRequest.newBuilder().setAge(45).setSize(14).setStrName("BonjourJava").setWeight(45).build());
          System.out.println(reply.toString());
          channel.shutdown();
     }
