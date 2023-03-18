6722113e66bbed71ff0656ad7a9a4c36bcb61113
Pierrea86 <54989357+Pierrea86@users.noreply.github.com>
2023-03-18 14:33:55 +0100

    Commentaires

--- src/main/java/ServeurApps.java
+++ src/main/java/ServeurApps.java
@@ -3,34 +3,7 @@ import imcPersonne.imcResponse;
 import io.grpc.stub.StreamObserver;
 
 public class ServeurApps extends imcPersonne.imcServiceGrpc.imcServiceImplBase {
-    //private int port;
-
-
-
-    //private Server server;
-
-   /* public int getPort() {
-        return port;
-    }
-
-    public Server getServer() {
-        return server;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public void setServer(Server server) {
-        this.server = server;
-    }
-    public ServeurApps(int port, Server server) {
-        super();
-
-        this.port = port;
-        this.server = server;
-    }*/
-public String imcCalcul(float weight,float size){
+public String imcCalcul(float weight,float size){//Fonction de calcul de l'imc d'une personne
     float imc=(weight)/(size*size);
     if(imc < 18.5){
         return "votre imc est : "+ imc+", il est inférieur à 18.5, vous êtes considèrez comme maigre";
@@ -45,13 +18,13 @@ public String imcCalcul(float weight,float size){
 }
 
     @Override
-    public void iMCRequest(imcPersonneRequest request, StreamObserver<imcResponse> responseObserver) {
-        String imc=imcCalcul(request.getWeight(),request.getSize());
-        String display= new StringBuilder().append("Bonjour, Monsieur ").append(request.getStrName()).append("\n vous êtes agé de ").append(request.getAge()).append(", ayant une taille de ").append(request.getSize()).append(", et un poids de ").append(request.getWeight()).append("\n"+imc).toString();
-        imcPersonne.imcResponse test= imcPersonne.imcResponse.newBuilder().setResponse(display).build();
-        System.out.println(test.toString());
-        responseObserver.onNext(test);
-        responseObserver.onCompleted();
+    public void iMCRequest(imcPersonneRequest request, StreamObserver<imcResponse> responseObserver) {//Implémentation gRPC pour calcul de l'imc
+        String imc=imcCalcul(request.getWeight(),request.getSize());//Calcul de l'imc
+        String display= new StringBuilder().append("Bonjour, Monsieur ").append(request.getStrName()).append("\n vous êtes agé de ").append(request.getAge()).append(", ayant une taille de ").append(request.getSize()).append(", et un poids de ").append(request.getWeight()).append("\n"+imc).toString();//Chaine de caractère pour affichage résultat
+        imcPersonne.imcResponse test= imcPersonne.imcResponse.newBuilder().setResponse(display).build();//creation de l'objet pour envoi de la réponse
+        System.out.println(test.toString());//Affichage de la réponse
+        responseObserver.onNext(test);//Envoi au client
+        responseObserver.onCompleted();//Traitement terminé
     }
 
 
