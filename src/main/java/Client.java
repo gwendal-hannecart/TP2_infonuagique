b3476c219ed922ba79c8479389adb4f71ba55e43
Pierrea86 <54989357+Pierrea86@users.noreply.github.com>
2023-03-18 16:16:21 +0100

    Commentaires

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -7,11 +7,11 @@ public class Client {
     private static final Logger logger = Logger.getLogger(Client.class.getName());
     public static void main(String[] args) {
         System.out.println("Bonjour");
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("grpc.stymi.fr", 50051).usePlaintext().build();
-        imcPersonne.imcServiceGrpc.imcServiceBlockingStub stub= imcPersonne.imcServiceGrpc.newBlockingStub(channel);
-        imcPersonne.imcResponse reply = stub.iMCRequest(imcPersonne.imcPersonneRequest.newBuilder().setAge(45).setSize(14).setStrName("BonjourJava").setWeight(45).build());
-         System.out.println(reply.toString());
-         channel.shutdown();
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("grpc.stymi.fr", 50051).usePlaintext().build();// Création du canal de communication avec le serveur gRPC
+        imcPersonne.imcServiceGrpc.imcServiceBlockingStub stub= imcPersonne.imcServiceGrpc.newBlockingStub(channel); // Création d'un stub, une classe générée automatiquement par gRPC à partir du fichier proto,qui permet d'appeler les méthodes définies dans le service distant
+        imcPersonne.imcResponse reply = stub.iMCRequest(imcPersonne.imcPersonneRequest.newBuilder().setAge(45).setSize(14).setStrName("BonjourJava").setWeight(45).build());//Appel de la méthode distante "iMCRequest" à travers le stub en passant les arguments nécessaires
+         System.out.println(reply.toString());// Affichage de la réponse retournée par le serveur
+         channel.shutdown();// Fermeture du canal de communication
     }
 
 }
