977aba7cbe828834538b947251c3fd2f2ee0a6ee
gwend <gwendal.hannecart@gmail.com>
2023-03-17 21:04:42 -0400

    Client Java modification et nouveau protobuf

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -7,15 +7,11 @@ public class Client {
     private static final Logger logger = Logger.getLogger(Client.class.getName());
     public static void main(String[] args) {
         System.out.println("Bonjour");
-      // HelloWorld test= HelloWorld.newBuilder().setAge(14).setSize(14).setStrHello("Bonjour").setWeight(45).build();
-     ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8000).usePlaintext().build();
-
-
-        TestHelloWorldServiceGrpc.TestHelloWorldServiceBlockingStub stub =  TestHelloWorldServiceGrpc.newBlockingStub(channel);
-       // StreamObserver<HelloWorld> responseObserver = new StreamObserver<HelloWorld>();
-        HelloResponse hello = stub.testHelloWorld(HelloWorld.newBuilder().setAge(14).setSize(14).setStrHello("Bonjour").setWeight(45).build());
-        System.out.println(hello.toString());
-        channel.shutdown();
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("grpc.stymi.fr", 50051).usePlaintext().build();
+        imcServiceGrpc.imcServiceBlockingStub stub= imcServiceGrpc.newBlockingStub(channel);
+        imcResponse reply = stub.iMCRequest(imcPersonneRequest.newBuilder().setAge(45).setSize(14).setStrName("BonjourJava").setWeight(45).build());
+         System.out.println(reply.toString());
+         channel.shutdown();
     }
 
 }
