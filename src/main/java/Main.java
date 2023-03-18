fac2878cbcd482e9aa9d1d95a7a301c5230d0e1b
Pierrea86 <54989357+Pierrea86@users.noreply.github.com>
2023-03-18 14:27:31 +0100

    Commentaires

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,11 +12,11 @@ import java.net.SocketAddress;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Server server;
-        SocketAddress address = new InetSocketAddress("192.168.0.65",  50051);
+        SocketAddress address = new InetSocketAddress("192.168.0.65",  50051);//Cree adresse de socket en utilisant l'ip et le numéro de port
       //  SocketAddress address = new InetSocketAddress("192.168.2.120",  8000);
-        server =  NettyServerBuilder.forAddress(address).addService(new ServeurApps()).build();
-        server.start();
-        System.out.println("Serveur start");
-        server.awaitTermination();
+        server =  NettyServerBuilder.forAddress(address).addService(new ServeurApps()).build();//cree le serveur gRPC en utilisant adresse socket
+        server.start();//démarre le serveur
+        System.out.println("Serveur start");//Affiche "serveur start" quand le serveur à démarré"
+        server.awaitTermination(); //Arrete le serveur gRPC quand le programme se termine
     }
 }
